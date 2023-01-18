98b97052e7be7bd39d65fd25e35a2c3350bc0dbb
id251 <danyalider178@gmail.com>
2023-01-18 19:05:02 +0300

    Commi

--- src/EnglishCourse.java
+++ src/EnglishCourse.java
@@ -1,28 +1,7 @@
 public class EnglishCourse extends Course {
-    public Course course;
+
     public EnglishCourse(String id, String studentName, String teacherName) {
         super(id, studentName, teacherName);
     }
-    public EnglishCourse(Course course) {
-        this.course = course;
-    }
 
-
-    @Override
-    public String toString() {
-        return "EnglishCourse{" + " id " + course.getId() + " student name: " + course.getStudentName() + " teacher name: "
-                + course.getTeacherName() +
-                "}";
-
-    }
-
-
-
-    public Course getCourse() {
-        return course;
-    }
-
-    public void setCourse(Course course) {
-        this.course = course;
-    }
 }
--- src/ItCourse.java
+++ src/ItCourse.java
@@ -1,31 +1,11 @@
 public class ItCourse extends Course {
-    public Course course;
+
     public ItCourse(String id, String studentName, String teacherName) {
         super(id, studentName, teacherName);
     }
   public ItCourse(){
 
   }
-  public ItCourse (Course course){
-        this.course=course;
-  }
 
 
-    @Override
-    public String toString() {
-        return "ItCourse{" + " id: " +course.getId() + " student name: " + course.getStudentName() + " teacher name: " + course.getTeacherName() +
-                "}";
-    }
-
-
-
-
-
-    public Course getCourse() {
-        return course;
-    }
-
-    public void setCourse(Course course) {
-        this.course = course;
-    }
 }
